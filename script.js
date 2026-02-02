774e59f754e5f6c973b8a0372d790bb6871e90b8
Juhi <65873340+Juhiiiii@users.noreply.github.com>
2026-02-02 15:13:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const noBtn = document.getElementById("no");
+const yesBtn = document.getElementById("yes");
+const message = document.getElementById("message");
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * 200 - 100;
+  const y = Math.random() * 200 - 100;
+
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+});
+
+yesBtn.addEventListener("click", () => {
+  message.innerHTML = "Yayyy! 💖 I knew you’d say yes 😍";
+});
